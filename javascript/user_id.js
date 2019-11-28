808d2ddb50aa7671594d5641cabf74dd31bb5ff9
Jon Briones <jonbriones@live.ca>
2019-11-28 00:28:14 -0800

    Updated a lot of things

--- javascript/user_id.js
+++ javascript/user_id.js
@@ -1,20 +1,18 @@
-firebase.auth().onAuthStateChanged(function (user) {
+function createUser() {
+  console.log(firebase.auth().currentUser);
+    firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         console.log(user.displayName);
-        document.getElementById("test").innerHTML = user.displayName;
-        user.once('value', function (snapshot) {
-          if (snapshot.exists()) {
-            console.log('exists');
-          } else {
-            db.collection("users").add({
-              "name": user.displayName,
-              "email": user.email,
-              "userid": user.uid});
-          }
+        document.getElementById("name").innerHTML = user.displayName;
+        db.collection("users").add({
+          "name": user.displayName,
+          "email": user.email,
+          "userid": user.uid
         });
       } else {
         console.log(user);
       }
-});
+    });
+}
 
 
